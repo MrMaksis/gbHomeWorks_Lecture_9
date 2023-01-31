e12866c176950d75dea4b32890f5a0db10cd848c
mr_maksis <max.morink@yandex.ru>
2023-01-31 14:26:01 +0300

    Task 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿void ShowNums(int num)
+{
+    Console.Write(num + " ");
+    if(num > 1) ShowNums(num - 1);
+}
+
+Console.Write("Please input num> ");
+ShowNums(Convert.ToInt32(Console.ReadLine()));
