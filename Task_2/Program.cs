17b7b80ab4bfbf0e6854db3038e809c78be2f09a
mr_maksis <max.morink@yandex.ru>
2023-01-31 14:56:07 +0300

    Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NaturalNumSumm(int m, int n, int sum)
+{
+    if(m > n) { Console.WriteLine($"Сумма натуральных элементов: {sum}"); return; }
+    sum = sum + (m++);
+    NaturalNumSumm(m,n,sum);
+}
+
+Console.Write("Input num M> ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input num N> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumSumm(m,n,0);
