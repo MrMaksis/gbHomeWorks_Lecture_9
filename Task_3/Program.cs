1131cc1c9c5d34a738205843c30f59cb572ae2c7
mr_maksis <max.morink@yandex.ru>
2023-02-01 20:25:39 +0300

    Task 3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+Console.Write("Введите первое число> ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана: {Ack(m,n)}");
